a6b0b38ab80010a508d4b7098faf71bffc054a85
Marcos Sánchez <188787196+Mynastique@users.noreply.github.com>
2025-12-26 12:00:09 +0100

    swagger para probar la api

--- ClinicaEsteticien/ClinicaEsteticen.Infrastructure/obj/Debug/net8.0/ClinicaEsteticen.Infrastructure.AssemblyInfo.cs
+++ ClinicaEsteticien/ClinicaEsteticen.Infrastructure/obj/Debug/net8.0/ClinicaEsteticen.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClinicaEsteticen.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a8b1cde8fb4112ca30478b1e37bd118946105eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27e71021f2f7bfb467cf71a6ffe566d40f52fbbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClinicaEsteticen.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClinicaEsteticen.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ClinicaEsteticien/ClinicaEsteticien/Program.cs
+++ ClinicaEsteticien/ClinicaEsteticien/Program.cs
@@ -1,25 +1,33 @@
+﻿using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ClinicaEsteticen API", Version = "v1" });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "ClinicaEsteticen API v1");
+        c.RoutePrefix = "swagger";
+    });
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapGet("/api/health", () => Results.Ok(new { status = "ok" })); 
 
+app.MapRazorPages();
 app.Run();
